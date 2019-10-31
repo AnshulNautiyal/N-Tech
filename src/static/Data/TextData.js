5883b913dc44a8348e586156e9ed6337799a12cd
Anshul Nautiyal <anshulnautiyaldeo@gmail.com>
2019-10-31 10:47:25 +0530

    Add Landing Page Data

--- src/static/Data/TextData.js
+++ src/static/Data/TextData.js
@@ -1,4 +1,9 @@
 export const INTRO = {
   heading: "Work Your Way With N-Tech Services",
-  subHeading: "You bring the skill. We'll make earning easy"
+  subHeading: "You bring the skill. We'll make earning easy",
+  noOfButton: [
+    {
+      btnName: "LEARN MORE"
+    }
+  ]
 };
