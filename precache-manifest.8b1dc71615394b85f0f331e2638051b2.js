3fa967e192ee6445b68c599ad303aeea07850787
Anshul Nautiyal <anshulnautiyaldeo@gmail.com>
2019-11-02 15:57:45 +0530

    Updates

---
+++ precache-manifest.8b1dc71615394b85f0f331e2638051b2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120835b4c084baa8d80f7915709e1da5",
+    "url": "/AnshulNautiyal/N-Tech/index.html"
+  },
+  {
+    "revision": "1b924034d49bc9f81615",
+    "url": "/AnshulNautiyal/N-Tech/static/css/main.5ee6ad26.chunk.css"
+  },
+  {
+    "revision": "51afe11f99e3d468c4a3",
+    "url": "/AnshulNautiyal/N-Tech/static/js/2.087ae2a6.chunk.js"
+  },
+  {
+    "revision": "1b924034d49bc9f81615",
+    "url": "/AnshulNautiyal/N-Tech/static/js/main.448e81b5.chunk.js"
+  },
+  {
+    "revision": "4c986e2990725aa63c56",
+    "url": "/AnshulNautiyal/N-Tech/static/js/runtime-main.e3c02e78.js"
+  },
+  {
+    "revision": "34e2b1c80f026c6f02610d0dff9fd2e6",
+    "url": "/AnshulNautiyal/N-Tech/static/media/LogoPNG.34e2b1c8.png"
+  },
+  {
+    "revision": "456e135e6a88bc4de849db980ade2ae1",
+    "url": "/AnshulNautiyal/N-Tech/static/media/LogoPNG2.456e135e.png"
+  },
+  {
+    "revision": "2de6c5749a05c869077aef588f74052f",
+    "url": "/AnshulNautiyal/N-Tech/static/media/work.2de6c574.png"
+  },
+  {
+    "revision": "b5ce801f16514c784ac05e1135b477f3",
+    "url": "/AnshulNautiyal/N-Tech/static/media/work2.b5ce801f.png"
+  }
+]);
