431b7d9698b36aa28928ef102b08a0aff32ec58e
Anshul Nautiyal <anshulnautiyaldeo@gmail.com>
2019-12-29 00:59:01 +0530

    Reset form after succesfull Submission

--- src/components/registration-component/registration-ajax.js
+++ src/components/registration-component/registration-ajax.js
@@ -6,11 +6,14 @@ export const handleSubmit = userInput => event => {
   event.preventDefault();
   const apiEndPoint =
     "https://sheltered-stream-06916.herokuapp.com/professional";
+
+  const formTag = event.target;
   axios
     .post(apiEndPoint, userInput)
     .then(function() {
       document.body.style.overflow = "auto";
       document.getElementById("modal").style.display = "none";
+      formTag.reset();
     })
     .catch(function(error) {
       console.log(error);
